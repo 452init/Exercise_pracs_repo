818ff6864ce655257098f684b55ca9c87d55d1f3
452init <clinsmol10@gmail.com>
2025-07-11 19:59:52 +0300

    Pangram problem

--- exercism/python/pangram/pangram.py
+++ exercism/python/pangram/pangram.py
@@ -1,7 +1,4 @@
 def is_pangram(sentence):
     unique_values = "".join(filter(str.isalpha, sentence))
     no_duplicates = set(unique_values.lower())
-    if len(no_duplicates) >= 26:
-        return True
-    else:
-        return False
+    return len(no_duplicates) >= 26
