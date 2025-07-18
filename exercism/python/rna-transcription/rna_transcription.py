6319123c54bf9bcbf775908862061323cda13975
452init <clinsmol10@gmail.com>
2025-07-18 11:34:07 +0300

    DNA to RNA Conversion

--- exercism/python/rna-transcription/rna_transcription.py
+++ exercism/python/rna-transcription/rna_transcription.py
@@ -1,14 +1,3 @@
 def to_rna(dna_strand):
-	rna_strand = ''
-	for i in dna_strand:
-		if i == 'G':
-			rna_strand += 'C'
-		elif i == 'C':
-			rna_strand += 'G'
-		elif i == 'T':
-			rna_strand += 'A'
-		elif i == 'A':
-			rna_strand += 'U'
-		else:
-		 	return rna_strand
-	return rna_strand
+	rna_strand = str.maketrans('GCTA', 'CGAU')
+	return dna_strand.translate(rna_strand)
