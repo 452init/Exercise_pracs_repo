510c05133abf9d3efe42d070d456091e39cabd64
452init <clinsmol10@gmail.com>
2025-07-26 15:09:41 +0300

    cleaned

--- exercism/python/resistor-color-expert/resistor_color_expert.py
+++ exercism/python/resistor-color-expert/resistor_color_expert.py
@@ -1,8 +1,9 @@
 def resistor_label(colors):
-    string_digit_band = ''
-    num_digit_band = 0
-    multiplied_digit_band = 0
-    correct_digit_band = 0
+    digit_count = 0
+    base_value = 0
+    total = 0
+    multiplier_color = ''
+    tolerance_color = ''
     string_correct_digit_band = ''
     
     digit_mapping = {'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'violet': 7, 'grey': 8, 'white': 9}
@@ -13,44 +14,35 @@ def resistor_label(colors):
 
     if len(colors) == 1:
         return '0 ohms'
-    elif len(colors) == 2 or len(colors) == 3:
-        raise TypeError ('Invalid band type.')
-    elif len(colors) == 4:
-        formatted_value = ''
-        string_digit_band += str(digit_mapping.get(colors[0]))
-        string_digit_band += str(digit_mapping.get(colors[1]))
-        num_digit_band = int(string_digit_band)
-        multiplied_digit_band = num_digit_band * (multiplier_mapping.get(colors[2]))
-        if multiplied_digit_band >= 1000000:
-            correct_digit_band = multiplied_digit_band//1000000
-            string_correct_digit_band = (str(correct_digit_band) + ' megaohms' + ' ±')
-            string_correct_digit_band += tolerance_mapping.get(colors[3])
-        elif multiplied_digit_band >= 1000:
-            correct_digit_band = multiplied_digit_band/1000
-            if correct_digit_band % 1 == 0:
-                formatted_value = str(int(correct_digit_band))
-            else:
-                formatted_value = f"{correct_digit_band:.1f}"
-            string_correct_digit_band = (str(formatted_value) + ' kiloohms' + ' ±')
-            string_correct_digit_band += tolerance_mapping.get(colors[3])
+    elif len(colors) in (2, 3):
+        raise TypeError('Invalid band type.')
+    else:
+        digit_count = 2 if len(colors) == 4 else 3
+        digit_string = ''
+        for i in range(digit_count):
+            digit_string += str(digit_mapping[colors[i]])
+        base_value = int(digit_string)
+    
+        multiplier_color = colors[digit_count]
+        tolerance_color = colors[-1]
+    
+        total = base_value * multiplier_mapping[multiplier_color]
+
+        if total >= 1_000_000:
+            value = total/1_000_000
+            unit = 'megaohms'
+        elif total >= 1_000:
+            value = total/1_000
+            unit = 'kiloohms'
         else:
-            string_correct_digit_band = (str(multiplied_digit_band) + ' ohms' + ' ±')
-            string_correct_digit_band += tolerance_mapping.get(colors[3])
-    elif len(colors) == 5:
-        string_digit_band += str(digit_mapping.get(colors[0]))
-        string_digit_band += str(digit_mapping.get(colors[1]))
-        string_digit_band += str(digit_mapping.get(colors[2]))
-        num_digit_band = int(string_digit_band)
-        multiplied_digit_band = num_digit_band * (multiplier_mapping.get(colors[3]))
-        if multiplied_digit_band >= 1000000:
-            correct_digit_band = multiplied_digit_band/1000000
-            string_correct_digit_band = (str(correct_digit_band) + ' megaohms' + ' ±')
-            string_correct_digit_band += tolerance_mapping.get(colors[4])
-        elif multiplied_digit_band >= 1000:
-            correct_digit_band = multiplied_digit_band/1000
-            string_correct_digit_band = (str(correct_digit_band) + ' kiloohms' + ' ±')
-            string_correct_digit_band += tolerance_mapping.get(colors[4])
+            value = total
+            unit = 'ohms'
+        if isinstance(value, float) and not value.is_integer():
+            formatted_value = f"{value:.2f}".rstrip('0').rstrip('.')
         else:
-            string_correct_digit_band = (str(multiplied_digit_band) + ' ohms' + ' ±')
-            string_correct_digit_band += tolerance_mapping.get(colors[4])
+            formatted_value = str(int(value))
+
+        string_correct_digit_band = (
+            formatted_value + ' ' + unit + ' ±' + tolerance_mapping[tolerance_color]
+            )
     return string_correct_digit_band
