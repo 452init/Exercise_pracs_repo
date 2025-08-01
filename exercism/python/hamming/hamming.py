080141f4eb14868bed2462a384417b6896b52085
452init <clinsmol10@gmail.com>
2025-08-01 20:10:03 +0300

    shortenned hamming number solution

--- exercism/python/hamming/hamming.py
+++ exercism/python/hamming/hamming.py
@@ -1,12 +1,6 @@
 def distance(strand_a, strand_b):
-	hamming_distance = 0
 
 	if len(strand_a) != len(strand_b):
 		raise ValueError('Strands must be of equal length.')
 	else:
-		for i in range(len(strand_a)):
-			if strand_a[i] == strand_b[i]:
-				continue
-			else:
-				hamming_distance += 1
-		return hamming_distance
+		return sum(x != y for x, y in zip(strand_a, strand_b))
