9e877ecac8707425fa679bd83886320a80371310
452init <clinsmol10@gmail.com>
2025-08-13 13:18:36 +0300

    Cleaned Code

--- exercism/python/etl/etl.py
+++ exercism/python/etl/etl.py
@@ -1,5 +1,6 @@
 def transform(legacy_data):
-	my_list_dict = []
-	for key, value in legacy_data.items():
-		my_list_dict.extend((i.lower(), key) for i in value)
-	return dict(my_list_dict)
+	return {
+			i.lower(): key
+			for key, value in legacy_data.items()
+			for i in value
+		}
