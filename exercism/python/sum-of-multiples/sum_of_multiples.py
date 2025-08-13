566172df06c16da91baf1f780fd7db83ac4e07f1
452init <clinsmol10@gmail.com>
2025-08-13 21:25:14 +0300

    Cleaned code

--- exercism/python/sum-of-multiples/sum_of_multiples.py
+++ exercism/python/sum-of-multiples/sum_of_multiples.py
@@ -1,8 +1,7 @@
 def sum_of_multiples(limit, multiples):
 	set_of_multiples = set()
 	for num in multiples:
-		if num == 0:
-			continue
-		for unique in range(num, limit, num):
-			set_of_multiples.add(unique)
+		if num:
+			for unique in range(num, limit, num):
+				set_of_multiples.add(unique)
 	return sum(set_of_multiples)
