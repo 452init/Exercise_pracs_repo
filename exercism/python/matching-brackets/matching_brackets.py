126063ae6df4543096763f71a87b66d458fa6e66
452init <clinsmol10@gmail.com>
2025-08-25 11:47:59 +0300

    Cleaner Version

--- exercism/python/matching-brackets/matching_brackets.py
+++ exercism/python/matching-brackets/matching_brackets.py
@@ -1,17 +1,25 @@
 def is_paired(input_string):
 	stack = []
-	openning_braces = '{[('
-	closing_braces = '])}'
-	mapping_braces = {'(': ')', '{': '}', '[': ']'}
+	mapped_braces = {'(': ')', '[': ']', '{': '}'}
+	mismatched_braces = ''
 
 	for char in input_string:
-		if char in openning_braces:
+		if char in mapped_braces.keys():
 			stack.append(char)
-		elif char in closing_braces:
+		elif char not in mapped_braces.keys() and char not in mapped_braces.values() and char == '\\':
+			continue
+		elif char in mapped_braces.values():
 			if len(stack) == 0:
-				return False
-			if mapping_braces[stack[-1]] == char:
+				mismatched_braces += char
+				return not mismatched_braces
+			#Raises KeyError incase any object that is not an openning bracket is added to the stack
+			if mapped_braces[stack[-1]] == char:
 				stack.pop()
 			else:
 				return False
 	return len(stack) == 0
+
+
+#How would you handle this problem if you also needed to track the positions of mismatched brackets for error reporting?
+#What if you needed to support custom bracket pairs that could be defined at runtime?
+#How would you modify your approach if brackets could be escaped (like \[ shouldn't count)?
