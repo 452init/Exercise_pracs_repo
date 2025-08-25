2654a5abbfbf764df8846d607b3301171ccef79b
452init <clinsmol10@gmail.com>
2025-08-25 09:08:53 +0300

    Cleaner Version

--- exercism/python/matching-brackets/matching_brackets.py
+++ exercism/python/matching-brackets/matching_brackets.py
@@ -3,20 +3,15 @@ def is_paired(input_string):
 	openning_braces = '{[('
 	closing_braces = '])}'
 	mapping_braces = {'(': ')', '{': '}', '[': ']'}
-	
-	if input_string == '':
-		return True
-	for i in input_string:
-		if i in openning_braces:
-			stack.append(i)
-		elif i in closing_braces:
+
+	for char in input_string:
+		if char in openning_braces:
+			stack.append(char)
+		elif char in closing_braces:
 			if len(stack) == 0:
 				return False
-			if mapping_braces[stack[-1]] == i:
+			if mapping_braces[stack[-1]] == char:
 				stack.pop()
-			elif mapping_braces[stack[-1]] != i:
+			else:
 				return False
-	if len(stack) == 0:
-		return True
-	else:
-		return False
+	return len(stack) == 0
