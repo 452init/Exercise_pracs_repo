ad28785eea47bca2a4f344faaf332ebee68b6568
452init <clinsmol10@gmail.com>
2025-09-03 19:38:20 +0300

    Implement atbash cipher with letter mirroring

--- exercism/python/atbash-cipher/atbash_cipher.py
+++ exercism/python/atbash-cipher/atbash_cipher.py
@@ -1,20 +1,20 @@
-def mirror_func(mirror_version):
+def atbash_transform(mirror_version):
 	list_char = []
 
 	for char in mirror_version.lower():
 		if 'a' <= char <= 'z':
-			mirror = ord('z') - ord(char) + ord('a')
-			list_char.append(chr(mirror))
+			list_char.append(chr(ord('z') - ord(char) + ord('a')))
 		elif char.isdigit():
 			list_char.append(char)
-	char_string = ''.join(list_char)
-	return char_string
+		else:
+			continue
+	return ''.join(list_char)
 
 def encode(plain_text):
-	result_cipher = mirror_func(plain_text)
+	result_cipher = atbash_transform(plain_text)
 	return ' '.join(result_cipher[i:i+5] for i in range(0, len(result_cipher), 5))
 
 
 def decode(ciphered_text):
-	result_string = mirror_func(ciphered_text)
+	result_string = atbash_transform(ciphered_text)
 	return result_string
