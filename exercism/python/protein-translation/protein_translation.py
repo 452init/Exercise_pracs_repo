c3af6ce012c6076c0b91aa0f9c64ce84da97c763
452init <clinsmol10@gmail.com>
2025-09-16 20:19:10 +0300

    More cleaner version using takewhile and lambda function

--- exercism/python/protein-translation/protein_translation.py
+++ exercism/python/protein-translation/protein_translation.py
@@ -1,28 +1,30 @@
+from itertools import takewhile
 def proteins(strand):
-	rna_sequence = {
-			'Methionine': 'AUG',
-			'Tryptophan': 'UGG',
-			'Phenylalanine': ('UUU', 'UUC'),
-			'Leucine': ('UUA', 'UUG'),
-			'Serine': ('UCU', 'UCC', 'UCA', 'UCG'),
-			'STOP': ('UAA', 'UAG', 'UGA'),
-			'Tyrosine': ('UAU', 'UAC'),
-			'Cysteine': ('UGC', 'UGU')
+	RNA_SEQUENCE = {
+			'AUG': 'Methionine',
+			'UGG': 'Tryptophan',
+			'UUU': 'Phenylalanine',
+			'UUC': 'Phenylalanine',
+			'UUA': 'Leucine',
+			'UUG': 'Leucine',
+			'UCU': 'Serine',
+			'UCA': 'Serine',
+			'UCC': 'Serine',
+			'UCG': 'Serine',
+			'UAU': 'Tyrosine',
+			'UAC': 'Tyrosine',
+			'UGC': 'Cysteine',
+			'UGU': 'Cysteine'
 			}
 
-	codons = []
+	stop_codons = {'UAA', 'UAG', 'UGA'}
 	protein_names = []
 
-	if len(strand) < 3:
-		raise TypeError('Invalid DNA strand')
-	else:
-		for x in range(0, len(strand), 3):
-			codons.append(strand[x:x+3])
-			for i in codons:
-				if i in rna_sequence['STOP']:
-					break
-				for key, value in rna_sequence.items():
-					if i in value:
-						protein_names.append(key)
-				codons.clear()
-		return protein_names
+	codon = [strand[x:x+3] for x in range(0, len(strand), 3)]
+
+	valid_codons = takewhile(
+			lambda codon: codon not in stop_codons and codon in RNA_SEQUENCE,
+			codon
+			)
+	protein_names = [RNA_SEQUENCE[codon] for codon in valid_codons]
+	return protein_names
