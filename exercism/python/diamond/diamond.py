cea5cf3d52e4ca1a89e0fd647aedd7e35a69775b
452init <clinsmol10@gmail.com>
2025-09-17 21:02:35 +0300

    polished with helper function

--- exercism/python/diamond/diamond.py
+++ exercism/python/diamond/diamond.py
@@ -1,26 +1,24 @@
+def build_row(row_num, total_row, alphabets):
+	space_list = total_row*[' ']
+	center = total_row//2
+	letter_position = min(row_num, total_row-1-row_num)
+
+	left_index = center - letter_position
+	right_index = center + letter_position
+	space_list[left_index] = alphabets[letter_position]
+	space_list[right_index] = alphabets[letter_position]
+
+	return ''.join(space_list)
+
+import string
 def rows(letter):
-	alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	alphabets = string.ascii_uppercase
+
+	if letter not in alphabets:
+		raise TypeError('Not a valid input!')
+
 	letter_index = alphabets.find(letter)
+	total_rows = letter_index*2+1
+	result_list = ([build_row(rows, total_rows, alphabets) for rows in range(total_rows)])
 
-	diamond_size = letter_index*2+1
-	center = diamond_size//2
-	result_list = []
-
-	for row in range(diamond_size):
-		# Every time the loop runs, space_list is reassigned new spaces
-		space_list = diamond_size*[' ']
-
-		# The d variable holds the minimum number equivalent to the right letter for each row and builds up the diamond shape prototype
-		d = min(row, diamond_size-1-row)
-
-		# The variables hold position to which the letters go to
-		left_index = center - d
-		right_index = center + d
-
-		# The letters are placed at the right positon building up the diamond
-		space_list[left_index] = alphabets[d]
-		space_list[right_index] = alphabets[d]
-
-		alphabet_positions = ''.join(space_list)
-		result_list.append(alphabet_positions)
 	return result_list
