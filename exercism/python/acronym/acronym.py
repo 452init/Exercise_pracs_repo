3246805d18eb179b6d96b862471f081dc1256d12
452init <clinsmol10@gmail.com>
2025-10-03 00:00:33 +0300

    acronym with diferent implementations

--- exercism/python/acronym/acronym.py
+++ exercism/python/acronym/acronym.py
@@ -1,3 +1,23 @@
+# using the extend method '+=' which is much slower
+def abbreviate(words):
+    acronym = ''
+    lowercase = words.replace("'", "").lower()
+    titlecase = lowercase.title()
+    for acro in titlecase:
+        if acro.isupper():
+            acronym += acro
+    return acronym
+
+#using list comprehension
+def abbreviate(words):
+    acronym_list = []
+    lowercase = words.replace("'", "").lower()
+    titlecase = lowercase.title()
+    acronym_list = [acro for acro in titlecase if acro.isupper()]
+    return ''.join(acronym_list)
+
+
+#using replace and list comprehension
 def abbreviate(words):
     cleaned = words.replace('-', ' ').replace('_', ' ')
     word_list = cleaned.split()
