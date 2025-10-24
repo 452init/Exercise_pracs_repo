256a49b8221bbb26187fdcebcce49d8248f4fa37
452init <clinsmol10@gmail.com>
2025-10-24 09:07:34 +0300

    random name generator for robots with a helper function

--- exercism/python/robot-name/robot_name.py
+++ exercism/python/robot-name/robot_name.py
@@ -2,18 +2,23 @@ import random
 import string
 
 class Robot:
+    used_names = []
+
     def __init__(self):
-        self.name = self.random_name()
-        self.old_name = self.name
+        self.name = self._generate_unique_name()
 
     def random_name(self):
-        my_string = string.ascii_uppercase
-        rand_nums = random.randrange(100, 999)
-        rand_letters = ''.join(random.choices(my_string, k=2))
-        return rand_letters+str(rand_nums)
+        letters = ''.join(random.choices(string.ascii_uppercase, k=2))
+        numbers = f"{random.randrange(0, 1000):03d}"
+        return letters + numbers
 
     def reset(self):
-        self.name = self.random_name()
-        if self.old_name == self.name:
-            self.name = self.random_name()
+        self.name = self._generate_unique_name()
         return self.name
+
+    def _generate_unique_name(self):
+        new_name = self.random_name()
+        while new_name in Robot.used_names:
+            new_name = self.random_name()
+        Robot.used_names.append(new_name)
+        return new_name
