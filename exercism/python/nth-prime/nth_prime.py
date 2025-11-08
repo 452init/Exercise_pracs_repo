5a6ae53d4f643a03bb0a4f99319506c9dd19e20f
452init <clinsmol10@gmail.com>
2025-11-08 20:01:37 +0300

    improvement on the algorithm by elimination of even nums

--- exercism/python/nth-prime/nth_prime.py
+++ exercism/python/nth-prime/nth_prime.py
@@ -6,15 +6,17 @@ def prime(number):
     num_to_check = 2
     last_prime = 0
 
+    
     while count < number:
-        is_prime = True
-        for num in range(2, int(num_to_check**.5)+1):
-            if num_to_check % num == 0:
-                is_prime = False
-                break
-        if is_prime:
-            last_prime = num_to_check
-            count += 1
-        if count == number:
-            return last_prime
+        if num_to_check == 2 or num_to_check % 2 != 0:
+            is_prime = True
+            for num in range(2, int(num_to_check**.5)+1):
+                if num_to_check % num == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                last_prime = num_to_check
+                count += 1
+            if count == number:
+                return last_prime
         num_to_check += 1
