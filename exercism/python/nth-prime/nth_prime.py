a90614e0aed77f0cbb100b88010a7abff995d0b4
452init <clinsmol10@gmail.com>
2025-11-08 18:56:06 +0300

    nth number

--- exercism/python/nth-prime/nth_prime.py
+++ exercism/python/nth-prime/nth_prime.py
@@ -1,2 +1,20 @@
 def prime(number):
-    pass
+    if number == 0:
+        raise ValueError('there is no zeroth prime')
+
+    count = 0
+    num_to_check = 2
+    last_prime = 0
+
+    while count < number:
+        is_prime = True
+        for num in range(2, int(num_to_check**.5)+1):
+            if num_to_check % num == 0:
+                is_prime = False
+                break
+        if is_prime:
+            last_prime = num_to_check
+            count += 1
+        if count == number:
+            return last_prime
+        num_to_check += 1
