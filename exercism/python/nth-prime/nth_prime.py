c0bfbe77584d44998daf4a1bdfdbdaf8d5127c2d
452init <clinsmol10@gmail.com>
2025-11-12 09:36:05 +0300

    using helper function to generate prime numbers

--- exercism/python/nth-prime/nth_prime.py
+++ exercism/python/nth-prime/nth_prime.py
@@ -1,22 +1,30 @@
 def prime(number):
     if number == 0:
         raise ValueError('there is no zeroth prime')
+    if number < 0:
+        raise ValueError('Cannot be a prime number!')
 
     count = 0
     num_to_check = 2
     last_prime = 0
-
     
     while count < number:
-        if num_to_check == 2 or num_to_check % 2 != 0:
+        if num_to_check == 2:
             is_prime = True
-            for num in range(2, int(num_to_check**.5)+1):
-                if num_to_check % num == 0:
-                    is_prime = False
-                    break
-            if is_prime:
-                last_prime = num_to_check
-                count += 1
-            if count == number:
-                return last_prime
-        num_to_check += 1
+            count += 1
+            last_prime = 2
+            num_to_check = 3
+            continue
+
+        if is_prime_number(num_to_check):
+            last_prime = num_to_check
+            count += 1
+        num_to_check += 2
+    return last_prime
+
+def is_prime_number(n):
+    is_prime = True
+    for num in range(2, int(n**.5)+1):
+        if n % num == 0:
+            return not is_prime
+    return is_prime
