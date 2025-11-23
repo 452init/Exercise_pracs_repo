4051faa7b1106004849243229d8a619ceb1b9c41
452init <clinsmol10@gmail.com>
2025-11-23 15:06:29 +0300

    Adjust gift ordering and day labeling in recite function

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -1,5 +1,10 @@
 def recite(start_verse, end_verse):
-    final_result = []
+    final_result = build_verse(start_verse, end_verse)
+
+    return final_result
+
+def build_verse(first_pos: int, last_pos: int) -> list:
+    result_list = []
     day_names = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh',
                  'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
     gift_descriptions = ['a Partridge in a Pear Tree', 'two Turtle Doves',
@@ -9,13 +14,12 @@ def recite(start_verse, end_verse):
                          'ten Lords-a-Leaping', 'eleven Pipers Piping',
                          'twelve Drummers Drumming']
 
-    for verse_num in range(start_verse, end_verse+1):
+    for verse_num in range(first_pos, last_pos+1):
         gift_list = []
         for gift_pos in range(verse_num):
             gift_list = [gift_descriptions[gift_pos]] + gift_list
         if verse_num > 1:
             modified_gift: str = 'and ' + gift_list[-1]
             gift_list[-1] = modified_gift
-
-        final_result.append(f"On the {day_names[verse_num-1]} day of Christmas my true love gave to me: {', '.join(gift_list)}.")
-    return final_result
+        result_list.append(f"On the {day_names[verse_num - 1]} day of Christmas my true love gave to me: {', '.join(gift_list)}.")
+    return  result_list
