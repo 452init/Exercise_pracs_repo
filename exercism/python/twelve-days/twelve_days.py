bb3605e8e489e643b23c9b3d74c7ef56e273d68c
452init <clinsmol10@gmail.com>
2025-11-25 06:31:12 +0300

    Adjust gift ordering and day labeling in recite function

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -1,25 +1,29 @@
-def recite(start_verse, end_verse):
-    final_result = build_verse(start_verse, end_verse)
+from typing import final
 
-    return final_result
-
-def build_verse(first_pos: int, last_pos: int) -> list:
-    result_list = []
-    day_names = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh',
+day_names = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh',
                  'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
-    gift_descriptions = ['a Partridge in a Pear Tree', 'two Turtle Doves',
+gift_descriptions = ['a Partridge in a Pear Tree', 'two Turtle Doves',
                          'three French Hens', 'four Calling Birds', 'five Gold Rings',
                          'six Geese-a-Laying', 'seven Swans-a-Swimming',
                          'eight Maids-a-Milking', 'nine Ladies Dancing',
                          'ten Lords-a-Leaping', 'eleven Pipers Piping',
                          'twelve Drummers Drumming']
 
-    for verse_num in range(first_pos, last_pos+1):
-        gift_list = []
-        for gift_pos in range(verse_num):
-            gift_list = [gift_descriptions[gift_pos]] + gift_list
-        if verse_num > 1:
-            modified_gift: str = 'and ' + gift_list[-1]
-            gift_list[-1] = modified_gift
-        result_list.append(f"On the {day_names[verse_num - 1]} day of Christmas my true love gave to me: {', '.join(gift_list)}.")
-    return  result_list
+def recite(start_verse, end_verse):
+    final_result = []
+    #final_gift_list = []
+
+    for verse in range(start_verse, end_verse+1):
+        gift_list_rev = [x for x in build_one_verse(verse)]
+        final_gift_list = gift_list_rev[-1]
+        if len(gift_list_rev) > 1:
+            modified_gift: str = 'and ' + final_gift_list[-1]
+            final_gift_list[-1] = modified_gift
+        final_result.append(f"On the {day_names[verse - 1]} day of Christmas my true love gave to me: {', '.join(final_gift_list)}.")
+    return final_result
+
+def build_one_verse(verse_num: int):
+
+    for gift in range(verse_num+1):
+        if gift > 0:
+            yield gift_descriptions[:gift][::-1]
