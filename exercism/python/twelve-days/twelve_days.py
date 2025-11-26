a9ac46e43e79709750d224e9ca3b3af920edf72b
452init <clinsmol10@gmail.com>
2025-11-26 06:43:07 +0300

    Adjust gift ordering and day labeling in recite function

--- exercism/python/twelve-days/twelve_days.py
+++ exercism/python/twelve-days/twelve_days.py
@@ -1,5 +1,3 @@
-from typing import final
-
 day_names = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh',
                  'eighth', 'ninth', 'tenth', 'eleventh', 'twelfth']
 gift_descriptions = ['a Partridge in a Pear Tree', 'two Turtle Doves',
@@ -13,16 +11,12 @@ def recite(start_verse, end_verse):
     final_result = []
 
     for verse in range(start_verse, end_verse+1):
-        gift_list_rev = [_ for _ in build_one_verse(verse)]
-        final_gift_list = gift_list_rev[-1]
+        gift_list_rev = build_one_verse(verse)
         if len(gift_list_rev) > 1:
-            modified_gift: str = 'and ' + final_gift_list[-1]
-            final_gift_list[-1] = modified_gift
-        final_result.append(f"On the {day_names[verse - 1]} day of Christmas my true love gave to me: {', '.join(final_gift_list)}.")
+            modified_gift: str = 'and ' + gift_list_rev[-1]
+            gift_list_rev[-1] = modified_gift
+        final_result.append(f"On the {day_names[verse - 1]} day of Christmas my true love gave to me: {', '.join(gift_list_rev)}.")
     return final_result
 
-def build_one_verse(verse_num: int):
-
-    for gift in range(verse_num+1):
-        if gift > 0:
-            yield gift_descriptions[:gift][::-1]
+def build_one_verse(verse_num: int) -> list:
+    return gift_descriptions[:verse_num][::-1]
