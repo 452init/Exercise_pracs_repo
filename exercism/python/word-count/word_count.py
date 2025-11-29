978011925a5e1561acc9ef2fcc821ce1fc702b2c
452init <clinsmol10@gmail.com>
2025-11-29 18:14:13 +0300

    cleaning words using regex then counting occurrence

--- exercism/python/word-count/word_count.py
+++ exercism/python/word-count/word_count.py
@@ -2,7 +2,7 @@ import re
 
 def count_words(sentence: str)->dict:
     partially_cleaned_sentence = re.sub(r"[^a-z0-9'_,\s]+", "", sentence.lower())
-    semi_cleaned = re.sub(r"[,_\n]+", ' ', partially_cleaned_sentence)
+    semi_cleaned = re.sub(r"[,_\s]+", ' ', partially_cleaned_sentence)
     quotes_before_word_removed = re.sub(r"(?<![a-z0-9])'+(?=[a-z0-9])", "", semi_cleaned)
     quotes_after_word_removed = re.sub(r"(?<=[a-z0-9])'+(?![a-z0-9])", "", quotes_before_word_removed)
     list_sentence = quotes_after_word_removed.split()
