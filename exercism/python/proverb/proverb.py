d411d4cb4e6323f2ffd092be12361e08b235a560
452init <clinsmol10@gmail.com>
2025-12-14 21:37:38 +0300

    unpacking args and kwargs

--- exercism/python/proverb/proverb.py
+++ exercism/python/proverb/proverb.py
@@ -1,2 +1,18 @@
-def proverb():
-    pass
+def proverb(*args, qualifier=None) -> list:
+    result = []
+    length = len(args)
+
+    if not length:
+        return result
+    elif length == 1 and qualifier:
+        result.append(f"And all for the want of a {qualifier} {args[0]}.")
+    elif length == 1:
+        result.append(f"And all for the want of a {args[0]}.")
+    else:
+        for value in range(length-1):
+            result.append(f"For want of a {args[value]} the {args[value+1]} was lost.")
+        if not qualifier:
+            result.append(f"And all for the want of a {args[0]}.")
+        else:
+            result.append(f"And all for the want of a {qualifier} {args[0]}.")
+    return result
