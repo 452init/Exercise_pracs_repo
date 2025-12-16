d09211947281fbc074e9cc5fa16e69a4039ab2a8
452init <clinsmol10@gmail.com>
2025-12-16 13:52:24 +0300

    Simplification using zip

--- exercism/python/proverb/proverb.py
+++ exercism/python/proverb/proverb.py
@@ -1,18 +1,13 @@
-def proverb(*args, qualifier=None) -> list:
-    result = []
-    length = len(args)
+def proverb(*args, qualifier=None) -> list[str]:
 
-    if not length:
-        return result
-    elif length == 1 and qualifier:
-        result.append(f"And all for the want of a {qualifier} {args[0]}.")
-    elif length == 1:
-        result.append(f"And all for the want of a {args[0]}.")
-    else:
-        for value in range(length-1):
-            result.append(f"For want of a {args[value]} the {args[value+1]} was lost.")
-        if not qualifier:
-            result.append(f"And all for the want of a {args[0]}.")
-        else:
-            result.append(f"And all for the want of a {qualifier} {args[0]}.")
+    result: list = []
+
+    if not args:
+        return  result
+    result = [f"For want of a {current} the {next_item} was lost." for current, next_item in zip(args, args[1:])]
+
+    items = args[0] if qualifier is None else f"{qualifier} {args[0]}"
+
+    result.append(f"And all for the want of a {items}.")
+
     return result
