5e7a953210d9eb589b64b2e9880c60436538e19a
452init <clinsmol10@gmail.com>
2025-12-23 20:28:44 +0300

    refactored logic

--- exercism/python/luhn/luhn.py
+++ exercism/python/luhn/luhn.py
@@ -3,29 +3,19 @@ class Luhn:
         self.card_num = card_num
 
     def valid(self):
-        """splits the string and into individual characters"""
+        #splits the string and into individual characters
         string_num = "".join(self.card_num.split())
 
-        """branch for checking validity of the string"""
+        #branch for checking validity of the string
         if len(string_num) < 2 or not string_num.isdigit():
             return False
         else:
-            """changes the strings to ints and reverse the list"""
+            #changes the strings to ints and reverse the list
             integer_list = [int(i) for i in string_num]
             integer_list = list(reversed(integer_list))
 
-            for i in range(1, len(integer_list), 2): """doubls the required values in the list"""
-                integer_list[i] *= 2
+            #enumerates the integer_list then doubles the required values in the list and subtracts 9 if the doubled num is greater than 9
+            result = [((num * 2) - 9 if num * 2 > 9 else (num * 2)) if idx % 2 !=0 else num for idx, num in enumerate(integer_list)]
 
-            """subtracts the values which are greater than 9 go get the luhns algorithm correct"""
-
-            result = []
-            for doubled in integer_list:
-                if doubled > 9:
-                    doubled -=9
-                    result.append(doubled)
-                else:
-                    result.append(doubled)
-
-            """sums the result and checks for mod 10 validity"""
+            #sums the result and checks for mod 10 validity
             return sum(result) % 10 == 0
