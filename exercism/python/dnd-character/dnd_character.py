207bd84a13704bd1aaf17708ddfc1aacfdc1f402
452init <clinsmol10@gmail.com>
2025-12-24 06:18:33 +0300

    dnd game using classes

--- exercism/python/dnd-character/dnd_character.py
+++ exercism/python/dnd-character/dnd_character.py
@@ -1,6 +1,24 @@
+import random
 class Character:
-    def __init__(self):
-        pass
+	def __init__(self):
+		self.strength = self.roll_dice()
+		self.dexterity = self.roll_dice()
+		self.constitution = self.roll_dice()
+		self.intelligence = self.roll_dice()
+		self.wisdom = self.roll_dice()
+		self.charisma = self.roll_dice()
+
+		self.modifier = modifier(self.constitution)
+		self.hitpoints =  self.modifier + 10
+
+
+	def roll_dice(self):
+		roll = [random.randint(1, 6) for _ in range(4)]
+		roll.remove(min(roll))
+		return sum(roll)
+
+	def ability(self):
+		return self.roll_dice()
 
 def modifier(value):
-    pass
+	return (value-10)//2
