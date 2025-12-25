540d0e4633e852f63a289d7cae6770a221e773b2
452init <clinsmol10@gmail.com>
2025-12-25 21:12:24 +0300

    refactored luhn algorithm

--- exercism/python/luhn/luhn.py
+++ exercism/python/luhn/luhn.py
@@ -9,13 +9,17 @@ class Luhn:
         #branch for checking validity of the string
         if len(string_num) < 2 or not string_num.isdigit():
             return False
-        else:
-            #changes the strings to ints and reverse the list
-            integer_list = [int(i) for i in string_num]
-            integer_list = list(reversed(integer_list))
 
-            #enumerates the integer_list then doubles the required values in the list and subtracts 9 if the doubled num is greater than 9
-            result = [((num * 2) - 9 if num * 2 > 9 else (num * 2)) if idx % 2 !=0 else num for idx, num in enumerate(integer_list)]
+        #changes the strings to ints and reverse the list
+        integer_list = [int(i) for i in string_num][::-1]
 
-            #sums the result and checks for mod 10 validity
-            return sum(result) % 10 == 0
+        #Helper function for doubling odd number digits
+        def luhn_double(digit):
+            doubled = digit * 2
+            return doubled - 9 if doubled > 9 else doubled
+
+        #Resturns all the digits in required order by luhn's algorithm
+        result = [luhn_double(num) if idx % 2 !=0 else num for idx, num in enumerate(integer_list)]
+
+        #sums the result and checks for mod 10 validity
+        return sum(result) % 10 == 0
