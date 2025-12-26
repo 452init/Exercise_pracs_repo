2f85d47f667c47d035ff94294562d237372035c4
452init <clinsmol10@gmail.com>
2025-12-26 22:04:38 +0300

    Refactored using generators

--- exercism/python/luhn/luhn.py
+++ exercism/python/luhn/luhn.py
@@ -10,16 +10,16 @@ class Luhn:
         if len(string_num) < 2 or not string_num.isdigit():
             return False
 
-        #changes the strings to ints and reverse the list
-        integer_list = [int(i) for i in string_num][::-1]
+        #changes the strings to ints and return reversed generator
+        generator = (int(i) for i in string_num[::-1])
 
         #Helper function for doubling odd number digits
         def luhn_double(digit):
             doubled = digit * 2
             return doubled - 9 if doubled > 9 else doubled
 
-        #Resturns all the digits in required order by luhn's algorithm
-        result = [luhn_double(num) if idx % 2 !=0 else num for idx, num in enumerate(integer_list)]
+        #Generates all the digits in required order by luhn's algorithm
+        result_generator = (luhn_double(num) if idx % 2 !=0 else num for idx, num in enumerate(generator))
 
         #sums the result and checks for mod 10 validity
-        return sum(result) % 10 == 0
+        return sum(result_generator) % 10 == 0
