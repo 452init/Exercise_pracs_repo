deb3d4e653744ee3781920dd378448c56e0dd1c0
452init <clinsmol10@gmail.com>
2026-01-08 22:26:33 +0300

    code refactor

--- exercism/python/dnd-character/dnd_character.py
+++ exercism/python/dnd-character/dnd_character.py
@@ -1,24 +1,19 @@
 import random
 class Character:
 	def __init__(self):
-		self.strength = self.roll_dice()
-		self.dexterity = self.roll_dice()
-		self.constitution = self.roll_dice()
-		self.intelligence = self.roll_dice()
-		self.wisdom = self.roll_dice()
-		self.charisma = self.roll_dice()
+		self.strength = self.ability()
+		self.dexterity = self.ability()
+		self.constitution = self.ability()
+		self.intelligence = self.ability()
+		self.wisdom = self.ability()
+		self.charisma = self.ability()
 
-		self.modifier = modifier(self.constitution)
-		self.hitpoints =  self.modifier + 10
-
-
-	def roll_dice(self):
-		roll = [random.randint(1, 6) for _ in range(4)]
-		roll.remove(min(roll))
-		return sum(roll)
+		self.constitution_modifier = modifier(self.constitution)
+		self.hitpoints =  self.constitution_modifier + 10
 
 	def ability(self):
-		return self.roll_dice()
+		result = [random.randint(1, 5) for _ in range(3)]
+		return sum(result)
 
 def modifier(value):
 	return (value-10)//2
