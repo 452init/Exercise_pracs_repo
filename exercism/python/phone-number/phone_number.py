b69c0ff05481c13fcd6dde2093cd1d9e10bdba38
452init <clinsmol10@gmail.com>
2026-01-15 22:35:57 +0300

    changed

--- exercism/python/phone-number/phone_number.py
+++ exercism/python/phone-number/phone_number.py
@@ -3,12 +3,12 @@ import string
 class PhoneNumber:
     def __init__(self, number):
         self.number = number
-
+        
         self.result = ''.join(num for num in self.number if num.isdigit())
             
         if any(n.isalpha() for n in self.number):
             raise ValueError("letters not permitted")
-        if len(self.result) < 10:
+        if len(self.number) < 10:
             raise ValueError("must not be fewer than 10 digits")
         if len(self.result) > 11:
             raise ValueError("must not be greater than 11 digits")
@@ -22,9 +22,9 @@ class PhoneNumber:
             raise ValueError("area code cannot start with zero")
         if self.result[0] == '1' or len(self.result) == 11 and self.result[1] == '1':
             raise ValueError("area code cannot start with one")
-        if any(l in string.punctuation() for l in self.number):
+        if any(l in string.punctuation for l in self.number):
             raise ValueError("punctuations not permitted")
         if len(self.result) == 11 and self.result[0] == '1':
-            return self.result[1:]
+            self.cleaned = self.result[1:]
         if len(self.result) == 10:
-            return self.result
+            self.cleaned = self.result
