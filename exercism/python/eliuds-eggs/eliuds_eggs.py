c1a4062f114b421161acce29629ae2f29b910078
452init <clinsmol10@gmail.com>
2026-02-04 20:57:12 +0300

    refactoring using bitwise operations

--- exercism/python/eliuds-eggs/eliuds_eggs.py
+++ exercism/python/eliuds-eggs/eliuds_eggs.py
@@ -1,8 +1,6 @@
 def egg_count(display_value):
-    string = ''
-    while (display_value > 0):
-        rem = display_value % 2
-        string = str(rem) + string
-        display_value = display_value//2
-    egg_no = [(int(n)) for n in string if n == '1']
-    return sum(egg_no)
+    count = 0
+    while display_value > 0:
+        count += display_value & 1
+        display_value >>= 1
+    return count
