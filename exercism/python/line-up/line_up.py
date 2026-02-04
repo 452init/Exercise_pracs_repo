47fdee0f33a03ce7a868abfb2e240faad58f8e65
452init <clinsmol10@gmail.com>
2026-02-04 20:15:52 +0300

    Code refactor

--- exercism/python/line-up/line_up.py
+++ exercism/python/line-up/line_up.py
@@ -1,9 +1,11 @@
 def line_up(name, number):
-    if str(number) == '1' or len(str(number)) > 1 and str(number)[-2] != '1' and str(number)[-1] == '1':
-        return f'{name}, you are the {number}st customer we serve today. Thank you!'
-    elif str(number) == '2' or len(str(number)) > 1 and str(number)[-2] != '1' and str(number)[-1] == '2':
-        return f'{name}, you are the {number}nd customer we serve today. Thank you!'
-    elif str(number) == '3' or len(str(number)) > 1 and str(number)[-2] > '1' and str(number)[-1] == '3':
-        return f'{name}, you are the {number}rd customer we serve today. Thank you!'
+    if len(str(number))>1 and str(number)[-2] != '1' and str(number)[-1] != '0' or number < 4:
+        if str(number)[-1] == '1':
+            suffix = 'st'
+        elif str(number)[-1] == '2':
+            suffix = 'nd'
+        elif str(number)[-1] == '3':
+            suffix = 'rd'
     else:
-        return f'{name}, you are the {number}th customer we serve today. Thank you!'
+        suffix = 'th'
+    return f'{name}, you are the {number}{suffix} customer we serve today. Thank you!'
