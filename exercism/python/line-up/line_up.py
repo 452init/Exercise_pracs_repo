9c4968a6a2aff8501422168037da71d858f61f9d
452init <clinsmol10@gmail.com>
2026-02-06 16:23:49 +0300

    Code refactor

--- exercism/python/line-up/line_up.py
+++ exercism/python/line-up/line_up.py
@@ -1,6 +1,5 @@
 def line_up(name, number):
     mapping = {'1': 'st', '2': 'nd', '3': 'rd'}
-    suffix = mapping.get(str(number)[-1], 'th')
-    if 11 <= number % 100 <= 13:
-        suffix = 'th'
+    if number<=3 or number<=10 and str(number)[-2] != '1':
+        suffix = mapping.get(str(number)[-1], 'th')
     return f'{name}, you are the {number}{suffix} customer we serve today. Thank you!'
