321aa7cc528dc63fa60f38f1ed9bcf2413eb2d6d
452init <clinsmol10@gmail.com>
2026-02-11 21:40:53 +0300

    refactor

--- exercism/python/transpose/transpose.py
+++ exercism/python/transpose/transpose.py
@@ -3,7 +3,13 @@ from itertools import zip_longest
 def transpose(text):
     list_of_lines = text.splitlines()
 
-    transposed_tuples = list(zip_longest(*list_of_lines, fillvalue=''))
+    transposed_tuples = list(zip_longest(*list_of_lines, fillvalue=' '))
 
-    transposed = '\n'.join(''.join(row) if len(row)>1 else row for row in transposed_tuples)
-    return transposed
+    processed_rows = []
+    for indx, row in enumerate(transposed_tuples):
+        row_string = ''.join(row)
+        
+        if indx == len(transposed_tuples)-1:
+            row_string = row_string.rstrip()
+        processed_rows.append(row_string)
+    return '\n'.join(processed_rows)
