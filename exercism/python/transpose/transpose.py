ed3aaae84283234ec05cf6d8d679d60119afc690
452init <clinsmol10@gmail.com>
2026-02-13 13:02:37 +0300

    Code correction and refactor

--- exercism/python/transpose/transpose.py
+++ exercism/python/transpose/transpose.py
@@ -1,18 +1,15 @@
 from itertools import zip_longest
-def transpose(text):
-    list_of_lines = text.splitlines()
-    lengths = sorted(len(line) for line in list_of_lines)
-    transposed_list = list(zip_longest(*list_of_lines, fillvalue=' '))
-    processed_rows = []
 
-    if len(lengths) < 2:
-            strip_from = len(transposed_list)-1
-    else:
-        strip_from = lengths[1]
+def transpose(text):
+    lines = text.splitlines()
+
+    max_len = 0
+    padded_lines = []
+    for line in reversed(lines):
+        max_len = max(max_len, len(line))
+        padded_lines.append(line.ljust(max_len))
+
+    lines = list(reversed(padded_lines))
+    transposed = zip_longest(*lines, fillvalue='')
     
-    for indx, row in enumerate(transposed_list):
-        row_string = ''.join(row)
-        if indx >= strip_from:
-            row_string = row_string.rstrip()
-        processed_rows.append(row_string)
-    return '\n'.join(processed_rows)
+    return '\n'.join(''.join(row) for row in transposed)
