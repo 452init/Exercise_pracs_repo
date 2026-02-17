a422a4dcc6a7e2d1482d612a4ab519fe2dbe66d5
452init <clinsmol10@gmail.com>
2026-02-17 12:51:36 +0300

    generator added

--- exercism/python/transpose/transpose.py
+++ exercism/python/transpose/transpose.py
@@ -1,15 +1,17 @@
-from itertools import zip_longest
+from itertools import zip_longest, chain
 
-def transpose(text):
+
+def transpose(text) -> str:
     lines = text.splitlines()
 
     max_len = 0
     padded_lines = []
     for line in reversed(lines):
         max_len = max(max_len, len(line))
-        padded_lines.append(line.ljust(max_len))
+
+        padded_lines.append(chain(line))
 
     lines = list(reversed(padded_lines))
     transposed = zip_longest(*lines, fillvalue='')
-    
+
     return '\n'.join(''.join(row) for row in transposed)
